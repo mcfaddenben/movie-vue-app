8d0d3dedb2b8d7475fa201acf94d34608fe44cbb
Ben McFadden <mcfaddenben64@gmail.com>
2021-06-04 02:07:31 -0400

    dds movie vue app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ Movies }}</h1>
+    <h2>Create a new movie</h2>
+    Title:
+    <input type="text" v-model="newTitle" />
+    Plot:
+    <input type="text" v-model="newPlot" />
+    Year:
+    <input type="text" v-model="newyYear" />
+    Director:
+    <input type="text" v-model="newDirector" />
+    <div v-for:="movie in movies" v-bind:key="movie.id">
+      <h3>{{ movie.title }} {{ movie.year }}</h3>
+      <button v-on:click="showInfo(movie)">More Info</button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      movie: [],
+      newTitle: "",
+      newPlot: "",
+      newYear: "",
+      newDirector: "",
+      currentMovie: {},
+    };
+  },
+  created: function () {
+    this.listMovies();
+  },
+  methods: {
+    listMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+        console.log(response.data);
+      });
+    },
+    addMovie: function () {
+      var params = {
+        title: this.newTitle,
+        plot: this.newPlot,
+        year: this.newYear,
+        director: this.newDirector,
+      };
+      axios
+        .post("http://localhost:3000/movies", params)
+        .then((response) => {
+          this.movies.push(response.data);
+          this.newTitle = "";
+          this.newPlot = "";
+          this.newYear = "";
+          this.newDirector = "";
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+    showInfo: function (movie) {
+      this.currentMovie = movie;
+      console.log(movie);
+      document.querySelector("#movie-detils").showModal();
+    },
   },
 };
 </script>
