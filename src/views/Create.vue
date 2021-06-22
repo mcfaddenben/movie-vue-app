291adc98333964657de8eb3ab7bbd6982abb3046
Ben McFadden <mcfaddenben64@gmail.com>
2021-06-22 02:42:08 -0400

    adds an english checkbox and placeholder text for inputs

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -9,11 +9,11 @@
       </ul>
       <div class="form-group">
         <label>Title:</label>
-        <input type="text" class="form-control" v-model="newMovieParams.title" />
+        <input type="text" class="form-control" v-model="newMovieParams.title" placeholder="Title"/>
       </div>
       <div class="form-group">
         <label>Plot:</label>
-        <input type="text" class="form-control" v-model="newMovieParams.plot" />
+        <input type="text" class="form-control" v-model="newMovieParams.plot" placeholder="Plot"/>
         <small v-if="newMovieParams.plot.length <= 300">{{ 300 - newMovieParams.plot.length }} characters left</small>
         <small v-else-if="newMovieParams.plot.lemgth > 300">
           Too many characters entered! Plot cannot be more than 300 characters
@@ -21,11 +21,15 @@
       </div>
       <div class="form-group">
         <label>year:</label>
-        <input type="text" class="form-control" v-model="newMovieParams.year" />
+        <input type="text" class="form-control" v-model="newMovieParams.year" placeholder="Year"/>
       </div>
       <div class="form-group">
         <label>director:</label>
-        <input type="text" class="form-control" v-model="newMovieParams.director" />
+        <input type="text" class="form-control" v-model="newMovieParams.director" placeholder="Director" />
+      </div>
+      <div class="form-group">
+        <label>director:</label>
+        <input type="checkbox" v-model="newMovieParams.english" />
       </div>
       <input type="submit" class="btn btn-primary" value="Submit" />
     </form>
